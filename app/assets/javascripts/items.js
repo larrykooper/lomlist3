998293e25009b0130f8b703289e47abf06f36dc2
Larry Kooper <larry.kooper@gmail.com>
2012-06-07 00:18:33 -0400

    interim commit for showing the item to be deleted in the confirmation box

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -4,6 +4,29 @@ $(document).ready(function() {
     var newtagslist = oldtagslist + ' ' + $(event.target).text().trim();
     $('#tag_list').val(newtagslist);
   });
-	
-});
 
+  $('#jq-dialog').dialog({
+      autoOpen: false,
+      width: 400,
+      modal: true,
+      resizable: false,
+      buttons: {
+        "OK": function() {
+          document.deleteItem.submit();
+        },
+        "Cancel": function() {
+          $(this).dialog("close");
+        }
+      }
+    });           
+  
+  	$('form#deleteItem').submit(function(e){
+      e.preventDefault();     
+      var nbrtodelete = $("input#nbrtodelete").val();
+      $("span#dialog-number strong").html(nbrtodelete);
+      var itemText = $("#text-"+nbrtodelete).html();
+      $("div#dialog-text").html(itemText.substring(0,350));
+      $('#jq-dialog').dialog('open');
+    });
+ 	
+});
