9838658a937cf54806b062e5ff8b770e99139ada
Larry Kooper <larry.kooper@gmail.com>
2012-06-09 00:29:54 -0400

    Fixed sorting of tags

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,16 +3,16 @@ class Label < ActiveRecord::Base
   self.table_name = 'tags'
 	
   def self.used_tags   
-    find_by_sql("select distinct t.name " +
+    find_by_sql("select distinct t.name, LOWER(t.name) " +
     	"from tags t inner join taggings tg " +
     	"on tg.tag_id = t.id " +
-    	"order by t.name")  
+    	"order by LOWER(t.name)")  
   end 
   
   def self.unused_tags 
     find_by_sql("select id, name from tags " +
     "where id not in (select tag_id from taggings) " +
-    "order by name")  
+    "order by LOWER(name)")  
   end 
 			
 end
