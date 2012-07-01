ab0b062a3282288cc304a632603e35366235f56f
Larry Kooper <larry.kooper@gmail.com>
2012-07-01 13:48:25 -0400

    Fixed setting initial invitation limit

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,3 +1,9 @@
 class Users::InvitationsController < Devise::InvitationsController
   
+  def create 
+    super 
+    self.resource.invitation_limit = self.resource.class.invitation_limit
+    self.resource.save
+  end
+  
 end
