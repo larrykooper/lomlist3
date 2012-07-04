05459b709bc18f6ad7e1991caa2fe9905690880b
Larry Kooper <larry.kooper@gmail.com>
2012-07-03 22:19:24 -0400

    fix to hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Lomlist3::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'larrybeth.textdriven.com' }
+  config.action_mailer.default_url_options = { :host => 'mydothis.com' }
 
   # Enable threaded mode
   # config.threadsafe!
